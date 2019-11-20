cde502e05556a7e8c98a042db1c7cfcedd508d37
Adam Pod <adam.podoxin@gmail.com>
2019-11-19 20:07:10 -0800

    Created basic node object

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Node : MonoBehaviour
+{
+    public int x;
+    public int y;
+
+    public SpriteValue spriteValue;
+
+    private Image image;
+
+    private void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    public void SetSpriteValue(SpriteValue spriteValue)
+    {
+        this.spriteValue = spriteValue;
+        image.sprite = spriteValue.sprite;
+    }
+}
