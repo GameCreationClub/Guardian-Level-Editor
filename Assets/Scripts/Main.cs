5a0d1228eea8be80348dfd0f5c0af7d78b942aa9
Adam Pod <adam.podoxin@gmail.com>
2019-11-19 19:59:45 -0800

    Created sprite list

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    public List<SpriteValue> spriteValues = new List<SpriteValue>();
+
+    public Sprite GetSpriteFromName(string name)
+    {
+        foreach (SpriteValue o in spriteValues)
+        {
+            if (o.name.Equals(name)) return o.sprite;
+        }
+
+        return null;
+    }
+
+    public string GetNameFromSprite(Sprite sprite)
+    {
+        foreach (SpriteValue o in spriteValues)
+        {
+            if (o.sprite.Equals(sprite)) return o.name;
+        }
+
+        return null;
+    }
+}
+
+[System.Serializable]
+public struct SpriteValue
+{
+    public string name;
+    public Sprite sprite;
+}
