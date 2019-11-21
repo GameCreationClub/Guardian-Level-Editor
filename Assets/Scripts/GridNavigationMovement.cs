ff0526812fbe384e157b0e090a216b17cbd7433f
Adam Pod <adam.podoxin@gmail.com>
2019-11-20 20:40:55 -0800

    Added grid navigation

---
+++ Assets/Scripts/GridNavigationMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridNavigationMovement : MonoBehaviour
+{
+    public bool canNavigate = true;
+
+    private void Update()
+    {
+        if (canNavigate)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(Vector3.down);
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(Vector3.up);
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(Vector3.left);
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(Vector3.right);
+            }
+
+            if (Input.GetAxisRaw("Mouse ScrollWheel") > 0f)
+            {
+                transform.localScale += Vector3.one * 0.1f;
+            }
+            else if (Input.GetAxisRaw("Mouse ScrollWheel") < 0f)
+            {
+                transform.localScale -= Vector3.one * 0.1f;
+            }
+        }
+    }
+}
