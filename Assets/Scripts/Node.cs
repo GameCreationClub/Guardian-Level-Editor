88ca99bcabd4d145547771b61f771feb78dcf1c3
Adam Pod <adam.podoxin@gmail.com>
2019-11-20 18:24:20 -0800

    Created basic node click handler

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -8,18 +8,28 @@ public class Node : MonoBehaviour
     public int x;
     public int y;
 
+    public new string name;
     public SpriteValue spriteValue;
 
     private Image image;
 
+    private NodeManager nodeManager;
+
     private void Start()
     {
         image = GetComponent<Image>();
+        nodeManager = FindObjectOfType<NodeManager>();
     }
 
     public void SetSpriteValue(SpriteValue spriteValue)
     {
         this.spriteValue = spriteValue;
         image.sprite = spriteValue.sprite;
+        name = spriteValue.name;
+    }
+
+    public void NodeClicked()
+    {
+        nodeManager.HandleNodeClick(this);
     }
 }
---
+++ Assets/Scripts/NodeManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeManager : MonoBehaviour
+{
+    private Main main;
+
+    private void Start()
+    {
+        main = FindObjectOfType<Main>();
+    }
+
+    public void HandleNodeClick(Node node)
+    {
+        node.SetSpriteValue(main.spriteValues[0]);
+    }
+}
